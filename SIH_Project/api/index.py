84eb9a8d4b823978173031485082bf329b008646
QuantaBoy <quantachanger@gmail.com>
2025-11-21 08:56:36 +0530

    trying to host

--- SIH_Project/api/index.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-# This is the entry point for Vercel serverless functions
-# Vercel expects the Flask app instance to be exposed
-handler = app
-
--- SIH_Project/app.py
+++ SIH_Project/app.py
@@ -44,4 +44,4 @@ def signup():
     return render_template('login_signup.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
